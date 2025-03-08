befccbcace988f04e5bd7e769dfb77f34c279819
Alouanne <aleanne.camire@gmail.com>
2025-03-08 17:25:17 -0500

    add database check for log in

--- src/main/java/com/heroku/java/logIn/LogInAPI.java
+++ src/main/java/com/heroku/java/logIn/LogInAPI.java
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class LogInAPI {
+    private final LogInRepository authRepository;
 
+    @Autowired
+    public LogInAPI(LogInRepository authRepository) {
+        this.authRepository = authRepository;
+    }
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
         if(checkAuthentification(username, password)) {
@@ -24,31 +29,23 @@ public class LogInAPI {
     }
     @GetMapping("/username")
     public ResponseEntity<LoginResponse> getLoginInfo() {
-        // Simuler un nom d'utilisateur (normalement récupéré depuis la base de données ou un token)
         String name = "John Doe";
 
-        // Créer la réponse
         LoginResponse response = new LoginResponse(name);
 
-        // Retourner la réponse avec un code 200
         return ResponseEntity.ok(response);
     }
-
-    // Classe interne représentant la réponse JSON
+    public boolean checkAuthentification(String username, String password){
+        if(authRepository.isUserValid(username,password)){
+            return true;
+        }else{
+            return false;
+        }
+    }
     public static class LoginResponse {
         private String name;
         public LoginResponse(String name) {
             this.name = name;
-        }
-    }
-
-    public boolean checkAuthentification(String username, String password){
-        String usernameCheck = "username1";
-        String passwordCheck = "password123";
-        if(username.equalsIgnoreCase(usernameCheck) && password.equalsIgnoreCase(passwordCheck)){
-            return true;
-        }else{
-            return false;
         }
     }
 }
