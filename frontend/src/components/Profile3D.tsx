3b24b4b5c9e47190f6668a3eb2e0f25d80d4510d
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-09 10:13:30 -0400

    add messenger + details

--- frontend/src/components/Profile3D.tsx
+++ frontend/src/components/Profile3D.tsx
@@ -4,27 +4,19 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 const Avatar3D: React.FC = () => {
     const { scene } = useGLTF("/models/skeleton.glb");
-
     return (
-        <primitive
-            object={scene}
-            scale={[2, 2, 2]}
-            position={[0, -2, 0]}
-        />
+        <primitive object={scene} scale={[2, 2, 2]} position={[0, -2, 0]} />
     );
 };
 
 const AvatarViewer: React.FC = () => {
     return (
         <Canvas>
-            {/* Lighting */}
-            <directionalLight position={[10, 10, 10]} intensity={1}/>
-            <hemisphereLight intensity={0.5}/>
-            <pointLight position={[5, 5, 5]} intensity={1}/>
-            {/* 3D Model */}
-            <Avatar3D/>
-            {/* Controls */}
-            <OrbitControls enableZoom={false}/>
+            <directionalLight position={[10, 10, 10]} intensity={1} />
+            <hemisphereLight intensity={0.5} />
+            <pointLight position={[5, 5, 5]} intensity={1} />
+            <Avatar3D />
+            <OrbitControls enableZoom={false} />
         </Canvas>
     );
 };
